c3e5d9c1c0d9d32c94276f04502119db0fe28a35
Akira-Wasabi <taka.akira.wasabi@gmail.com>
2016-05-13 15:04:13 +0200

    Update jquery.nanotoc2.js

--- assets/js/plugins/jquery.nanotoc2.js
+++ assets/js/plugins/jquery.nanotoc2.js
@@ -44,7 +44,7 @@
 
   jQuery.nanoToc2.defaultOptions = {
     widthContent : '75%',
-    selectors : 'h1, h2, h3, h4',
+    selectors : 'h2, h3, h4, h5',
     anchorPrefix : '',
     hideTocWidth : 600,
     borderColor : '#fb5',
